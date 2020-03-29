adeeec63a0de4dc4320151a3e954c80941f39f20
Bartek <bart.matusz@gmail.com>
2020-03-29 18:24:40 +0200

    multiple tag adding and search and display

--- src/main/java/com/photoarchive/services/PhotoAddingService.java
+++ src/main/java/com/photoarchive/services/PhotoAddingService.java
@@ -6,9 +6,12 @@ import com.photoarchive.models.PhotoDTO;
 import com.photoarchive.repositories.PhotoRepository;
 import com.photoarchive.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -29,9 +32,23 @@ public class PhotoAddingService {
     public Photo addPhoto(PhotoDTO photoDTO){
         Photo photo = new Photo();
         photo.setUrl(photoDTO.getUrl());
-        final Set<Tag> tags = tagParsingService.parseTagSet(photoDTO);
-        photo.setTags(tags);
-        tagRepository.saveAll(tags);
+        final Set<Tag> tagsFromDTO = tagParsingService.parseTagSet(photoDTO);
+
+
+        Set<Tag> tagsToAdd = new HashSet<>();
+        //photo.getTags().forEach(tag -> tag.setTag_name(stringValidatorService.handleTagInput(tag.getTag_name())));
+        tagsFromDTO.forEach(tag -> {
+            Optional<Tag> one = tagRepository.findOne(Example.of(tag));
+            if (one.isPresent()) {
+                tagsToAdd.add(one.get());
+            } else {
+                tagsToAdd.add(tag);
+            }
+        });
+
+        photo.setTags(tagsToAdd);
+
+        tagRepository.saveAll(photo.getTags());
         tagRepository.flush();
         return photoRepository.saveAndFlush(photo);
     }
