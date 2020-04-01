f32593e792e7db153ccd34e08d1b5e126124b5ec
Bartek <bart.matusz@gmail.com>
2020-04-01 23:22:52 +0200

    method postWithUrl validating url through java constrains

--- src/main/java/com/photoarchive/models/PhotoDTO.java
+++ src/main/java/com/photoarchive/models/PhotoDTO.java
@@ -2,13 +2,18 @@ package com.photoarchive.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 public class PhotoDTO {
 
+    @URL(message = "please set a valid url")
+    @NotBlank(message = "please set an url")
+    @Size(max = 255, message = "url is too long")
     private String url;
     @NotBlank(message = "please set minimum 1 tag")
     private String tagsAsString;
