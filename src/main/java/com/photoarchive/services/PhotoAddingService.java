f8032eee67f0bf5afbc1bca676873e2c6e76dc2d
Bartek <bart.matusz@gmail.com>
2020-04-02 00:58:25 +0200

    photoFieldsSetUpService class added

--- src/main/java/com/photoarchive/services/PhotoAddingService.java
+++ src/main/java/com/photoarchive/services/PhotoAddingService.java
@@ -1,22 +1,15 @@
 package com.photoarchive.services;
 
 import com.photoarchive.domain.Photo;
-import com.photoarchive.domain.Tag;
 import com.photoarchive.models.PhotoWithFileDTO;
 import com.photoarchive.models.PhotoWithUrlDTO;
 import com.photoarchive.repositories.PhotoRepository;
 import com.photoarchive.repositories.TagRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
 @Service
 @Slf4j
@@ -24,59 +17,33 @@ public class PhotoAddingService {
 
     private PhotoRepository photoRepository;
     private TagRepository tagRepository;
-    private TagParsingService tagParsingService;
-    private CloudinaryService cloudinaryService;
+    private PhotoFieldsSetUpService photoFieldsSetUpService;
 
     @Autowired
     public PhotoAddingService(PhotoRepository photoRepository,
                               TagRepository tagRepository,
-                              TagParsingService tagParsingService,
-                              CloudinaryService cloudinaryService) {
+                              PhotoFieldsSetUpService photoFieldsSetUpService) {
         this.photoRepository = photoRepository;
         this.tagRepository = tagRepository;
-        this.tagParsingService = tagParsingService;
-        this.cloudinaryService = cloudinaryService;
+        this.photoFieldsSetUpService = photoFieldsSetUpService;
     }
 
     public Photo addPhoto(final PhotoWithUrlDTO photoWithUrlDTO) {
         Photo photo = new Photo();
-        setCorrectTags(photo, photoWithUrlDTO.getTagsAsString());
         photo.setUrl(photoWithUrlDTO.getUrl());
+        photoFieldsSetUpService.setCorrectTags(photo, photoWithUrlDTO.getTagsAsString());
 
         return saveToDB(photo);
     }
 
     public Photo addPhoto(final PhotoWithFileDTO photoWithFileDTO) {
         Photo photo = new Photo();
-        setCorrectTags(photo, photoWithFileDTO.getTagsAsString());
-        setCorrectUrl(photo, photoWithFileDTO.getMultipartFile());
+        photoFieldsSetUpService.setCorrectUrl(photo, photoWithFileDTO.getMultipartFile());
+        photoFieldsSetUpService.setCorrectTags(photo, photoWithFileDTO.getTagsAsString());
 
         return saveToDB(photo);
     }
-
-    private void setCorrectTags(Photo photo, String tags) {
-        final Set<Tag> tagsFromInput = tagParsingService.parseTagSet(tags);
-
-        Set<Tag> tagsToAdd = new HashSet<>();
-
-        tagsFromInput.forEach(tag -> {
-            Optional<Tag> one = tagRepository.findOne(Example.of(tag));
-            if (one.isPresent()) {
-                tagsToAdd.add(one.get());
-            } else {
-                tagsToAdd.add(tag);
-            }
-        });
-        photo.setTags(tagsToAdd);
-    }
-
-    private void setCorrectUrl(Photo photo, MultipartFile multipartFile) {
-        Map result = cloudinaryService.upload(multipartFile);
-        String urlInCloud = (String) result.get("url");
-        System.out.println(urlInCloud);
-        photo.setUrl(urlInCloud);
-    }
-
+    
     @Transactional
     public Photo saveToDB(Photo photo){
         tagRepository.saveAll(photo.getTags());
---
+++ src/main/java/com/photoarchive/services/PhotoFieldsSetUpService.java
@@ -0,0 +1,55 @@
+package com.photoarchive.services;
+
+import com.photoarchive.domain.Photo;
+import com.photoarchive.domain.Tag;
+import com.photoarchive.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class PhotoFieldsSetUpService {
+
+    private TagParsingService tagParsingService;
+    private TagRepository tagRepository;
+    private CloudinaryService cloudinaryService;
+
+    @Autowired
+    public PhotoFieldsSetUpService(TagParsingService tagParsingService, TagRepository tagRepository, CloudinaryService cloudinaryService) {
+        this.tagParsingService = tagParsingService;
+        this.tagRepository = tagRepository;
+        this.cloudinaryService = cloudinaryService;
+    }
+
+    public Photo setCorrectTags(Photo photo, String tags) {
+        final Set<Tag> tagsFromInput = tagParsingService.parseTagSet(tags);
+
+        Set<Tag> tagsToAdd = new HashSet<>();
+
+        tagsFromInput.forEach(tag -> {
+            Optional<Tag> one = tagRepository.findOne(Example.of(tag));
+            if (one.isPresent()) {
+                tagsToAdd.add(one.get());
+            } else {
+                tagsToAdd.add(tag);
+            }
+        });
+        photo.setTags(tagsToAdd);
+        return photo;
+    }
+
+    public Photo setCorrectUrl(Photo photo, MultipartFile multipartFile) {
+        Map result = cloudinaryService.upload(multipartFile);
+        String urlInCloud = (String) result.get("url");
+        System.out.println(urlInCloud);
+        photo.setUrl(urlInCloud);
+        return photo;
+    }
+
+}
