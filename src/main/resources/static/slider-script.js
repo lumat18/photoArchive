178f49e1812c170a6d890171b7afc8f8c1be0373
lumat <lumat@op.pl>
2020-04-03 00:31:46 +0200

    add adding GUI

---
+++ src/main/resources/static/slider-script.js
@@ -0,0 +1,12 @@
+const slider = document.querySelector(".slider-switch");
+
+const urlForm = document.querySelector(".url");
+const fileForm = document.querySelector(".file");
+
+function slide(){
+    if(slider.checked){
+
+    }else{
+
+    }
+}
