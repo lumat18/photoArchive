e2b3fee17150d6d7fe412ca269fbf2d03a755b68
Bartek <bart.matusz@gmail.com>
2020-04-29 22:10:24 +0200

    mail properties set in application.yml

--- src/main/java/com/photoarchive/services/EmailService.java
+++ src/main/java/com/photoarchive/services/EmailService.java
@@ -2,24 +2,29 @@ package com.photoarchive.services;
 
 import com.photoarchive.domain.Token;
 import com.photoarchive.security.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    private static final String SUBJECT = "PhotoARCHive verification email";
 
     private TokenService tokenService;
+    private JavaMailSender javaMailSender;
 
-    @Autowired
-    public EmailService(TokenService tokenService) {
+    public EmailService(TokenService tokenService, JavaMailSender javaMailSender) {
         this.tokenService = tokenService;
+        this.javaMailSender = javaMailSender;
     }
 
     public void sendVerificationEmail(User user) {
         Token token = tokenService.createToken(user);
         String url = "http://localhost:8080/token?value=" + token.getValue();
 
-
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject();
     }
 
 
