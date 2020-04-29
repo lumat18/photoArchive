63a8696e8ed511c13e95b53e5688c863a1cacd47
Bartek <bart.matusz@gmail.com>
2020-04-29 21:57:27 +0200

    token service

--- src/main/java/com/photoarchive/services/EmailService.java
+++ src/main/java/com/photoarchive/services/EmailService.java
@@ -1,31 +1,22 @@
 package com.photoarchive.services;
 
-import com.photoarchive.domain.Token;
-import com.photoarchive.repositories.TokenRepository;
 import com.photoarchive.security.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class EmailService {
 
-    private TokenRepository tokenRepository;
+    private TokenService tokenService;
 
     @Autowired
-    public EmailService(TokenRepository tokenRepository) {
-        this.tokenRepository = tokenRepository;
+    public EmailService(TokenService tokenService) {
+        this.tokenService = tokenService;
     }
 
     public void sendVerificationEmail(User user) {
+        
     }
 
-    private Token createToken(User user){
-        Token token = new Token();
-        String value = UUID.randomUUID().toString();
-        token.setValue(value);
-        token.setUser(user);
-        return token;
-    }
+
 }
--- src/main/java/com/photoarchive/services/TokenService.java
+++ src/main/java/com/photoarchive/services/TokenService.java
@@ -1,7 +1,28 @@
 package com.photoarchive.services;
 
+import com.photoarchive.domain.Token;
+import com.photoarchive.repositories.TokenRepository;
+import com.photoarchive.security.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class TokenService {
+
+    private TokenRepository tokenRepository;
+
+    @Autowired
+    public TokenService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    public Token createToken(User user){
+        Token token = new Token();
+        String value = UUID.randomUUID().toString();
+        token.setValue(value);
+        token.setUser(user);
+        return tokenRepository.save(token);
+    }
 }
