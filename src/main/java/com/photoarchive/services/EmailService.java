da066fb7f32a6024f0215176228b3198c29a178e
Bartek <bart.matusz@gmail.com>
2020-04-29 22:21:53 +0200

    email service written and refactored

--- src/main/java/com/photoarchive/services/EmailService.java
+++ src/main/java/com/photoarchive/services/EmailService.java
@@ -2,11 +2,13 @@ package com.photoarchive.services;
 
 import com.photoarchive.domain.Token;
 import com.photoarchive.security.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
     private static final String SUBJECT = "PhotoARCHive verification email";
 
@@ -19,12 +21,22 @@ public class EmailService {
     }
 
     public void sendVerificationEmail(User user) {
-        Token token = tokenService.createToken(user);
-        String url = "http://localhost:8080/token?value=" + token.getValue();
+        SimpleMailMessage message = createMessageTo(user);
+        javaMailSender.send(message);
+        log.info("Verification email send to "+user.getEmail());
+    }
 
+    private SimpleMailMessage createMessageTo(User user){
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(user.getEmail());
-        message.setSubject();
+        message.setSubject(SUBJECT);
+        message.setText("In order to validate your account, please click the link below: \n" + createActivationLink(user));
+        return message;
+    }
+    
+    private String createActivationLink(User user){
+        Token token = tokenService.createToken(user);
+        return "http://localhost:8080/token?value=" + token.getValue();
     }
 
 
