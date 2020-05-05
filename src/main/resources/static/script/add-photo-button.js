d825ad7ad306bd0c3e1ba52923cb1f987f052209
lumat <lumat@op.pl>
2020-05-05 23:04:21 +0200

    upload.html refactor

--- src/main/resources/static/script/add-photo-button.js
+++ src/main/resources/static/script/add-photo-button.js
@@ -1,7 +1,3 @@
-// const slider = document.querySelector(".slider-switch");
-// const fileButton = document.querySelector(".file-button");
-// const urlButton = document.querySelector(".url-button");
-
 function clickCorrectButton() {
     if(document.querySelector(".slider-switch").checked){
         console.log("urlButton");
