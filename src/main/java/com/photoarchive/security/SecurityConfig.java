ad2cf8fb296ccb6547835f1430030bb01489b6f4
lumat <lumat@op.pl>
2020-05-07 01:06:51 +0200

    fix email-input padding

--- src/main/java/com/photoarchive/security/SecurityConfig.java
+++ src/main/java/com/photoarchive/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/search", "/upload")
+                .antMatchers("/search", "/upload", "/contact")
                 .authenticated()
                 .antMatchers("/", "/**")
                 .permitAll();
