7c84348f3b16417dec76042152565dbf37f32833
lumat <lumat@op.pl>
2020-05-10 15:33:02 +0200

    add processChangePassword method tests

--- src/test/java/com/photoarchive/controllers/ChangePasswordControllerTest.java
+++ src/test/java/com/photoarchive/controllers/ChangePasswordControllerTest.java
@@ -1,13 +1,16 @@
 package com.photoarchive.controllers;
 
+import com.photoarchive.domain.User;
 import com.photoarchive.managers.TokenManager;
 import com.photoarchive.managers.UserManager;
+import com.photoarchive.models.ChangePasswordDTO;
 import com.photoarchive.services.ResetCodeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,6 +19,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -43,6 +47,8 @@ class ChangePasswordControllerTest {
     private String resetCode;
     private String tokenValue;
     private LocalDateTime creationDate;
+    private User user;
+    private String newPassword;
 
     @BeforeEach
     void setUp() {
@@ -50,6 +56,8 @@ class ChangePasswordControllerTest {
         resetCode = "resetCode";
         tokenValue = "tokenValue";
         creationDate = LocalDateTime.now();
+        user = new User();
+        newPassword = "newPassword1";
     }
 
     @Test
@@ -76,8 +84,8 @@ class ChangePasswordControllerTest {
         when(tokenManager.existsByValue(tokenValue)).thenReturn(false);
 
         mockMvc.perform(MockMvcRequestBuilders
-        .get("/change")
-        .param("value", resetCode))
+                .get("/change")
+                .param("value", resetCode))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("message", INVALID_LINK_MESSAGE))
                 .andExpect(view().name("email-input"));
@@ -96,8 +104,8 @@ class ChangePasswordControllerTest {
         when(tokenManager.hasExpired(creationDate)).thenReturn(true);
 
         mockMvc.perform(MockMvcRequestBuilders
-        .get("/change")
-        .param("value", resetCode))
+                .get("/change")
+                .param("value", resetCode))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("message", EXPIRED_LINK_MESSAGE))
                 .andExpect(view().name("email-input"));
@@ -126,5 +134,47 @@ class ChangePasswordControllerTest {
         verify(tokenManager, times(1)).existsByValue(tokenValue);
         verify(tokenManager, times(1)).hasExpired(creationDate);
         verifyNoInteractions(userManager, passwordEncoder);
+    }
+
+    @Test
+    void shouldProcessPasswordChange() throws Exception {
+        when(resetCodeService.extractTokenValue(resetCode)).thenReturn(tokenValue);
+        when(userManager.loadUserByToken(tokenValue)).thenReturn(Optional.of(user));
+        when(passwordEncoder.encode(newPassword)).thenReturn(newPassword);
+        doNothing().when(userManager).setNewPassword(user, newPassword);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/change")
+                .param("password", newPassword)
+                .param("matchingPassword", newPassword)
+                .flashAttr("resetCode", resetCode))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("message", PASSWORD_CHANGED_MESSAGE))
+                .andExpect(view().name("login"));
+
+        verify(resetCodeService, times(1)).extractTokenValue(resetCode);
+        verify(userManager, times(1)).loadUserByToken(tokenValue);
+        verify(passwordEncoder, times(1)).encode(newPassword);
+        verify(userManager, times(1)).setNewPassword(user, newPassword);
+    }
+
+    @Test
+    void shouldNotProcessPasswordChange() throws Exception {
+        when(resetCodeService.extractTokenValue(resetCode)).thenReturn(tokenValue);
+        when(userManager.loadUserByToken(tokenValue)).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/change")
+                .param("password", newPassword)
+                .param("matchingPassword", newPassword)
+                .flashAttr("resetCode", resetCode))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("message", PASSWORD_CHANGED_MESSAGE))
+                .andExpect(view().name("login"));
+
+        verify(resetCodeService, times(1)).extractTokenValue(resetCode);
+        verify(userManager, times(1)).loadUserByToken(tokenValue);
+        verifyNoMoreInteractions(userManager);
+        verifyNoInteractions(passwordEncoder);
     }
 }
