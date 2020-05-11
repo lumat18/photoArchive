92ef1b9cad62e34b0d2b7db00528bbbab9f63d8f
lumat <lumat@op.pl>
2020-05-11 21:33:58 +0200

    add TokenManagerTest createTokenFor method test

--- src/test/java/com/photoarchive/managers/TokenManagerTest.java
+++ src/test/java/com/photoarchive/managers/TokenManagerTest.java
@@ -1,14 +1,21 @@
 package com.photoarchive.managers;
 
+import com.photoarchive.domain.Token;
+import com.photoarchive.domain.User;
 import com.photoarchive.repositories.TokenRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class TokenManagerTest {
@@ -19,25 +26,41 @@ class TokenManagerTest {
     @Autowired
     private TokenManager tokenManager;
 
+    @Captor
+    private ArgumentCaptor<Token> captor;
+
+    final Integer tokenExpirationTimeInHours = TokenManager.TOKEN_EXPIRATION_TIME_IN_HOURS;
+
     //hasExpired method
     @Test
-    void shouldReturnFalseIfCreationTimeWasLessThan24HoursAgo(){
+    void shouldReturnFalseIfCreationTimeWasLessThan24HoursAgo() {
         //given
-        LocalDateTime creationTime = LocalDateTime.now().minusHours(20);
+        LocalDateTime creationTime = LocalDateTime.now().minusHours(tokenExpirationTimeInHours - 1);
         //when
         boolean result = tokenManager.hasExpired(creationTime);
         //then
         assertFalse(result);
     }
+
     @Test
-    void shouldReturnTrueIfCreationTimeWasMoreThan24HoursAgo(){
+    void shouldReturnTrueIfCreationTimeWasMoreThan24HoursAgo() {
         //given
-        LocalDateTime creationTime = LocalDateTime.now().minusHours(25);
+        LocalDateTime creationTime = LocalDateTime.now().minusHours(tokenExpirationTimeInHours + 1);
         //when
         boolean result = tokenManager.hasExpired(creationTime);
         //then
         assertTrue(result);
     }
-    
 
+    @Test
+    void shouldCreateTokenForGivenUser(){
+        //given
+        final User user = new User();
+        when(tokenRepository.save(user.getToken())).thenReturn(null);
+        //when
+        tokenManager.createTokenFor(user);
+        //then
+        verify(tokenRepository).save(captor.capture());
+        assertThat(captor.getValue().getUser()).isEqualTo(user);
+    }
 }
