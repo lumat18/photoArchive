8b32ff8806b189a09de2e675f0eecb507bb3dae5
Bartek <bart.matusz@gmail.com>
2020-05-11 23:56:17 +0200

    searchController refactor

--- src/main/java/com/photoarchive/controllers/SearchController.java
+++ src/main/java/com/photoarchive/controllers/SearchController.java
@@ -2,10 +2,10 @@ package com.photoarchive.controllers;
 
 import com.photoarchive.domain.Photo;
 import com.photoarchive.domain.User;
+import com.photoarchive.models.UserInfo;
 import com.photoarchive.services.SearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,11 +16,10 @@ import java.util.Set;
 @Slf4j
 @Controller
 @RequestMapping("/search")
-@SessionAttributes("userInfo")
+@SessionAttributes({"userInfo", "foundPhotos"})
 public class SearchController {
 
     private SearchService searchService;
-    private Set<Photo> foundPhotos;
 
     @Autowired
     public SearchController(SearchService searchService) {
@@ -28,18 +27,17 @@ public class SearchController {
     }
 
     @GetMapping
-    public String getPhotos(Model model, @AuthenticationPrincipal User user){
+    public String getPhotos(@AuthenticationPrincipal User user, Model model){
         UserInfo userInfo = UserInfo.createUserInfo(user);
         model.addAttribute("userInfo", userInfo);
-        model.addAttribute("foundPhotos", foundPhotos);
         return "search";
     }
 
     @PostMapping("/find-photos")
-    public String processSearch(@RequestParam(name = "tagString") String tagString){
-        foundPhotos = searchService.getPhotosByTags(tagString);
+    public String processSearch(@RequestParam(name = "tagString") String tagString, Model model){
+        Set<Photo> foundPhotos = searchService.getPhotosByTags(tagString);
+        model.addAttribute("foundPhotos", foundPhotos);
         log.info("Photo found in database: " + foundPhotos);
-
-        return  "redirect:/search";
+        return  "search";
     }
 }
