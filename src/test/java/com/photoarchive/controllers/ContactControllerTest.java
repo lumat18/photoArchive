b092a8af045c2a0ce01e9f1a4e74134efc251e4b
lumat <lumat@op.pl>
2020-05-12 23:09:02 +0200

    add more RegistrationController tests

--- src/test/java/com/photoarchive/controllers/ContactControllerTest.java
+++ src/test/java/com/photoarchive/controllers/ContactControllerTest.java
@@ -1,5 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.photoarchive.controllers;
 
+import com.photoarchive.managers.UserManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(ContactController.class)
 class ContactControllerTest {
+    @MockBean
+    private UserManager userManager;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void shouldShowContactForm() throws Exception {
+        mockMvc.perform(get("/contact")
+                .sessionAttr("userInfo", new UserInfo()))
+                .andExpect(view().name("contact"));
+    }
 }
